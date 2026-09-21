b7af6fb5427fde2ed8185ed1e8ef40d38a06ec89
Isienov08 <idaira.sierra@gmail.com>
2026-09-21 20:03:05 +0200

    Añadida la practica de la gasolinera V2

--- Practica_gasolinera/src/ControladorMenu.java
+++ Practica_gasolinera/src/ControladorMenu.java
@@ -1,2 +1,22 @@
 public class ControladorMenu {
+
+    public void darAltaCliente() {
+        //objeto cliente
+        //existeFichero o crearFichero
+        //guadarEnFichero
+    }
+
+    public void listaClientes(){
+        //leerFichero
+    }
+
+    public void registrarPago() {
+        //objeto pago
+        //existeFichero o crearFichero
+        //guadarEnFichero
+    }
+
+    public void listaPagos(){
+        //leerFichero
+    }
 }
--- Practica_gasolinera/src/GestorFicheros.java
+++ Practica_gasolinera/src/GestorFicheros.java
@@ -1,2 +1,15 @@
 public class GestorFicheros {
+//    private path ruta;
+
+
+    public GestorFicheros() {
+        //llamare a existe y crear
+        //para que al instanciar la clase se cree automaticamente el fichero
+    }
+
+    public void existeFichero() {}
+    private void crearFichero() {}
+
+    public void guadarEnFichero() {}
+    public void leerFichero() {}
 }
